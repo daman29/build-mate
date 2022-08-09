1814309bf1a8460675a163f5ad0e7ca8aba2b280
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-08-09 21:44:37 +0930

    added Task model

--- server/models/Task.js
+++ server/models/Task.js
@@ -1,1 +1,37 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
+
+const taskSchema = new Schema({
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+  predecessor: {
+    type: Schema.Types.ObjectId,
+    ref: "Task",
+  },
+  successor: {
+    type: Schema.Types.ObjectId,
+    ref: "Task",
+  },
+  projectId: {
+    type: Schema.Types.ObjectId,
+    ref: "Project",
+    required: true,
+  },
+  assigneeId: {
+    type: Schema.Types.ObjectId,
+    ref: "Teammate",
+  },
+  priority: {
+    type: String,
+    enum: ["low", "mid", "high"],
+  },
+});
+
+const Task = model("Task", taskSchema);
+
+module.exports = Task;
