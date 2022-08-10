df7b616600a489aa1e4fa5ea622349d600f5eec1
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-08-10 16:17:20 +0930

    added teammate model

--- server/models/Teammate.js
+++ server/models/Teammate.js
@@ -1,1 +1,49 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
+
+const teammateSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: [
+      "Bricky",
+      "Sparky",
+      "Plumber",
+      "Concreter",
+      "Chippy",
+      "Architect",
+      "Engineer",
+      "Surveyor",
+      "Draftsperson",
+      "Supervisor",
+      "Certifier",
+      "Waterproofer",
+      "Roofer",
+      "Tiler",
+      "Painter",
+      "Landscaper",
+      "General",
+      "Other",
+    ],
+    required: true,
+  },
+  phoneNumber: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    match: [/.+@.+\..+/, "Must match an email address!"],
+  },
+  teamLeadId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const Teammate = model("Teammate", teammateSchema);
+
+module.exports = Teammate;
