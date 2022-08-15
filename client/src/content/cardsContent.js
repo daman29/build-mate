d3a421ac903bc7e0bd0981f581c3aa38d868404b
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-08-15 14:46:48 +0930

    added landing page cards content

---
+++ client/src/content/cardsContent.js
@@ -0,0 +1,22 @@
+const cardsContent = [
+  {
+    id: 1,
+    title: "Automated Project Creation",
+    body: "Build a project automatically with only a few inputs. The Project Creator is built using industry best practices. Be first time right and beat the market with this powerful tool.",
+    image: "",
+  },
+  {
+    id: 2,
+    title: "Team Builder",
+    body: "Efficiently manage your team and know the right person to contact when it all goes wrong. Never have to google in a moment of panic; knowing the right person is just a click away.",
+    image: "",
+  },
+  {
+    id: 3,
+    title: "Central Repository",
+    body: "A central repository for all documents from concept designs to survey reports breaks silos in the field and allows you to deliver the project at high quality without losing information.",
+    image: "",
+  },
+];
+
+export default cardsContent
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import
+
+import { Container } from '../styles/Container.styled'
+
+const Landing = () => {
+
+  return (
+    <Container>
+        {}
+    </Container>
+  );
+};
+
+export default Landing;
