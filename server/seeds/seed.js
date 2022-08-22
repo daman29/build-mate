58a9669e7563e08d92439686523640b34c549add
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-08-22 12:12:35 +0930

    optimised loading speeds. Created a PWA

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,21 +1,15 @@
 const db = require("../config/connection");
 const { User, Project, Task, Teammate } = require("../models");
 const userSeeds = require("./userSeeds.json");
-const projectSeeds = require("./projectSeeds.json");
-const taskSeeds = require("./taskSeeds.json");
 const teammateSeeds = require("./teammateSeeds.json");
 
 db.once("open", async () => {
   try {
     await User.deleteMany({});
-    await Project.deleteMany({});
     await Teammate.deleteMany({});
-    await Task.deleteMany({});
 
     await User.create(userSeeds);
-    await Project.create(projectSeeds);
     await Teammate.create(teammateSeeds);
-    await Task.create(taskSeeds);
   } catch (err) {
     console.error(err);
     process.exit(1);
